df5fe7a5f0471042d7f2861056cc2393ae681026
Migdaliz Mejias <mamejias@fullsail.edu>
2015-01-14 21:29:59 -0500

    Added
    I added variables and console logs

---
+++ project2.js
@@ -0,0 +1,15 @@
+// Migdaliz Mejias
+// SDI 
+
+var concString = "My name is " + " Migdaliz Mejias";
+
+console.log(concString);
+
+// My variables
+
+var alert = "alert";
+var payment = "payment";
+
+// My Outputs
+console.log("If tuition is late an " + alert + " will notify parents" + ".");
+console.log("So they can make a " + payment + " on time" + ".");
