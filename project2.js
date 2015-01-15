525e85182bc3c2a1bfe5cc485f266f5f5c9c1396
Migdaliz Mejias <mamejias@fullsail.edu>
2015-01-14 21:57:49 -0500

    Change
    Corrected line 13, did not make sense with payment variable.

--- project2.js
+++ project2.js
@@ -6,10 +6,13 @@ var concString = "My name is " + " Migdaliz Mejias";
 console.log(concString);
 
 // My variables
-
-var alert = "alert";
+var alert = "alert"; 
 var payment = "payment";
 
 // My Outputs
-console.log("If tuition is late an " + alert + " will notify parents" + ".");
+console.log("If tuition is approaching due date an " + alert + " will notify parents" + ".");
 console.log("So they can make a " + payment + " on time" + ".");
+
+// Conditions
+
+
